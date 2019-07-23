b3494b766b8d6eab4efbd1759a5843e863422144
Sean Gay <sean@sixtydigits.com>
2019-07-23 13:02:47 +1000

    Completed #16 - Conditional Output

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 const Ninjas = ({ninjas}) => {
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-      </div>
-    )
+    return ninja.age > 20 ? (
+      (
+        <div className="ninja" key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+        </div>
+      )
+    ) : null;
   });
   
   return (
     <div className="ninja-list">{ninjaList}</div>
   );
 };
+
 export default Ninjas;
