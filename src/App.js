3841b6ea682f3241f43c9c5ed955e21f925f6680
Sean Gay <sean@sixtydigits.com>
2019-07-23 13:49:32 +1000

    Completed #22 - Lifecycle methods

--- src/App.js
+++ src/App.js
@@ -31,6 +31,15 @@ export default class App extends Component {
     });
   };
   
+  
+  componentDidMount() {
+    console.log("Component mounted")
+  }
+  
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("COMPONENT UPDATED", prevProps, prevState)
+  }
+  
   render() {
     return (
       <div className="App">
