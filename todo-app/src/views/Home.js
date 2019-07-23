a101771835fac8b4ff1e17fd94b503e44db31bfe
Sean Gay <sean@sixtydigits.com>
2019-07-23 16:26:31 +1000

    Completed #33 - importing images

--- todo-app/src/views/Home.js
+++ todo-app/src/views/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
+import Pokeball from "../pokeball.png";
 
 export default class Home extends Component {
   state = {
@@ -20,9 +21,10 @@ export default class Home extends Component {
     const postList = posts.length ? (posts.map(post => {
       return (
         <div className="post card" key={post.id}>
+          <img src={Pokeball} alt="Poke-ball image"/>
           <div className="card-content">
             <Link to={`/${post.id}`}>
-              <span className="card-title">{post.title}</span>
+              <span className="card-title red-text">{post.title}</span>
             </Link>
             <p>{post.body}</p>
           </div>
@@ -32,7 +34,7 @@ export default class Home extends Component {
       <div className="center">No posts yet</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
