03d6b5cec70a98d71a0c7986d031c57c5f3076da
Sean Gay <sean@sixtydigits.com>
2019-07-23 12:47:45 +1000

    Completed #14 - Outputting lists

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Ninjas from './components/Ninjas';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>My first react app</h1>
-      <p>Welcome</p>
-      <Ninjas name="Ryu" age="25" belt="black"/>
-      <Ninjas name="Ken" age="21" belt="gold"/>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>My first react app</h1>
+        <p>Welcome</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
 }
