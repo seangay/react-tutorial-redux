f6c02f2eea26ea2703e5f23a74ff3585b52e8ecf
Sean Gay <sean@sixtydigits.com>
2019-07-24 10:35:39 +1000

    Completed #42 - Map dispatch to props

--- todo-app/src/views/Post.js
+++ todo-app/src/views/Post.js
@@ -2,16 +2,26 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push("/");
+  };
+  
   render() {
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
     );
-
+    
     return (
       <div className="container">
         {post}
@@ -26,4 +36,10 @@ const mapStateToProps = (state, ownProps) => {
     post: state.posts.find((post) => post.id === id)
   }
 };
-export default connect(mapStateToProps)(Post);
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => dispatch({type: "DELETE_POST", id: id})
+  }
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
