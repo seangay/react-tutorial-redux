933b45542e8488c54fd5fff48f352078fe64592a
Sean Gay <sean@sixtydigits.com>
2019-07-24 10:22:59 +1000

    Completed #41 - post detail page integration with redux

--- todo-app/src/views/Post.js
+++ todo-app/src/views/Post.js
@@ -1,25 +1,12 @@
 import React, {Component} from "react";
-import axios from "axios";
+import {connect} from "react-redux";
 
-export default class Post extends Component {
-  state = {
-    post: null
-  };
-  
-  async componentDidMount() {
-    let id = this.props.match.params.post_id;
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    
-    this.setState({
-      post: response.data
-    })
-  }
-  
+class Post extends Component {
   render() {
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -32,3 +19,11 @@ export default class Post extends Component {
     );
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params["post_id"];
+  return {
+    post: state.posts.find((post) => post.id === id)
+  }
+};
+export default connect(mapStateToProps)(Post);
